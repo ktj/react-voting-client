61845b8a675a4eb84713a42a3ad9f7d9e14fb452
Julius Räihä <julius.raiha@phz.fi>
2017-01-14 19:52:54 +0200

    Separated voting -> voting, vote, winner

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 const loki = (entry) => {console.log(entry)};
 ReactDOM.render(
-  <Voting pair={pair} vote={loki} />,
+  <Voting pair={pair} vote={loki} winner="Trainspotting"/>,
   document.getElementById('app')
 );

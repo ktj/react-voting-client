2e8b6fbc789d114e9a6c60a66bde234d17fbc1c6
Julius Räihä <julius.raiha@phz.fi>
2017-01-14 19:37:15 +0200

    Voting: tests & onClick cb

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
-
+const loki = (entry) => {console.log(entry)};
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} vote={loki} />,
   document.getElementById('app')
 );

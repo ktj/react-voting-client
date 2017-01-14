aa50fb66ebc4db4a159be1d3a612da1236770026
Julius Räihä <julius.raiha@phz.fi>
2017-01-14 20:12:51 +0200

    Added root route to voting component

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 const loki = (entry) => {console.log(entry)};
+
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
   <Voting pair={pair} vote={loki} winner="Trainspotting"/>,
   document.getElementById('app')

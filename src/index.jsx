659a17751fa2101de5b39613de29643ddea73b2d
Julius Räihä <julius.raiha@phz.fi>
2017-03-28 22:51:19 +0300

    Redux store setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
-const loki = (entry) => {console.log(entry)};
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 const routes = <Route component={App}>
-  <Route path="/" component={Voting} />
+	<Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
 </Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} vote={loki} winner="Trainspotting"/>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
